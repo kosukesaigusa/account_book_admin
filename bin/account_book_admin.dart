be729302d1af48c28483b1cb1036db1087fc68aa
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-01-21 14:34:03 +0900

    first commit

---
+++ bin/account_book_admin.dart
@@ -0,0 +1,5 @@
+import 'package:account_book_admin/account_book_admin.dart' as account_book_admin;
+
+void main(List<String> arguments) {
+  print('Hello world: ${account_book_admin.calculate()}!');
+}
---
+++ test/account_book_admin_test.dart
@@ -0,0 +1,8 @@
+import 'package:account_book_admin/account_book_admin.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
